ba580fa9a08c1a7f6538aaec3d6fd22d6ff9238e
Jessica Jiang <jessicajiang5@gmail.com>
2015-02-17 15:37:41 -0800

    crytographer

--- JMCh24_3Cryptogram/Enigma.java
+++ JMCh24_3Cryptogram/Enigma.java
@@ -16,39 +16,90 @@ import java.util.Arrays;
 public class Enigma
 {
     private char[] lookupTable;
+    //private static final String letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
     public Enigma(int numLetters)
     {
-        // TODO complete constructor
+        lookupTable = new char[numLetters];
+            
     }
 
     public void setSubstitution(int index, char ch)
     {
-        // TODO complete method
+        lookupTable[index] = ch;
     }
 
     public String decode(String text)
     {
-        StringBuffer buffer = new StringBuffer(text.length());
-
-        // TODO complete method
-
+        StringBuffer buffer = new StringBuffer( text.length() );
+        char character;
+        int i = 0;
+ 
+        for ( int index = 0; index < text.length(); index++ )
+        {
+            character = text.charAt( index );
+ 
+            if ( Character.isLowerCase( character ) )
+            {
+                i = Character.getNumericValue( text.charAt( index ) )
+                    - Character.getNumericValue( 'a' );
+                buffer.append( ( "" + lookupTable[i] ).toLowerCase() );
+            }
+            else if ( Character.isUpperCase( character ) )
+            {
+                i = Character.getNumericValue( text.charAt( index ) )
+                    - Character.getNumericValue( 'A' );
+                buffer.append( ( "" + lookupTable[i] ).toUpperCase() );
+            }
+            else
+            {
+                buffer.append( character );
+            }
+ 
+        }
         return buffer.toString();
     }
 
     public String getHints(String text, String lettersByFrequency)
     {
-        // TODO complete method
-
-        return new String(/* FIX THIS */);
+        String hints = "";
+        
+        int[] counts = countLetters( text );
+        int rank;
+ 
+        for ( int k = 0; k < counts.length; k++ )
+        {
+            rank = 0;
+ 
+            for ( int i = 0; i < counts.length; i++ )
+            {
+                if ( counts[i] < counts[k] || counts[i] == counts[k] && i < k )
+                {
+                    rank++;
+                }
+            }
+            hints += lettersByFrequency.charAt( rank );
+        }
+        return hints;
     }
 
     private int[] countLetters(String text)
     {
         int[] counts = new int[lookupTable.length];
-
-        // TODO complete method
-
+        
+        for ( char c : text.toCharArray() )
+        {
+            if ( c >= 'A' && c <= 'Z' )
+            {
+                counts[c - 'A']++;
+ 
+            }
+            else if ( c >= 'a' && c <= 'z' )
+            {
+                counts[c - 'a']++;
+            }
+        }
+ 
         return counts;
     }
 
