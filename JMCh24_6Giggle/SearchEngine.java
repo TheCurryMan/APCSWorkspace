1cbe9b7e7b1a132ded16580cd5278db1264010ac
Jessica Jiang <jessicajiang5@gmail.com>
2015-02-26 17:16:55 -0800

    giggle

--- JMCh24_6Giggle/SearchEngine.java
+++ JMCh24_6Giggle/SearchEngine.java
@@ -5,10 +5,10 @@ import java.util.*;
    TODO Follow it with additional details about its purpose, what abstraction
    it represents, and how to use it.
 
-   @author  TODO Your Name
-   @version TODO Date
+   @author  Jessica Jiang
+   @version February 24, 2015
 
-   @author Period - TODO Your Period
+   @author Period - 4
    @author Assignment - JM24.6 - Search Engine
 
    @author Sources - TODO list collaborators
@@ -20,10 +20,55 @@ public class SearchEngine
     private Map<String, List<String>> myIndex; // holds the word index
 
     // Constructor(s)
-
-
+    public SearchEngine( String url ) 
+    { 
+        myURL = url; 
+        myIndex = new HashMap( 20000 ); 
+    } 
+   
+   
+    public String getURL() 
+    { 
+        return myURL; 
+    } 
+ 
     // Methods
-
+   
+    public void add( String line ) 
+    { 
+        Set<String> list = parseWords( line ); 
+        for ( String string : list ) 
+        { 
+            LinkedList<String> temp = new LinkedList<String>(); 
+            if ( !myIndex.containsKey( string ) ) 
+            { 
+                myIndex.put( string, temp ); 
+            } 
+            myIndex.get( string ).add( line ); 
+        } 
+    } 
+   
+   
+    public List<String> getHits( String word ) 
+    { 
+        return myIndex.get( word ); 
+    } 
+   
+   
+    private Set<String> parseWords( String line ) 
+    { 
+        String[] temp = line.split( "\\W+" ); 
+        TreeSet<String> words = new TreeSet<String>(); 
+        for ( String string : temp ) 
+        { 
+            if ( !string.isEmpty() ) 
+            { 
+                string = string.toLowerCase();
+                words.add( string );
+            } 
+        } 
+        return words; 
+    } 
 
     //*************************************************************
     // For test purposes only
